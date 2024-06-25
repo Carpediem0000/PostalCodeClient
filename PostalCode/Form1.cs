f55b0c2bda8b36b2fae05f3fd33b943097e69a5d
Ваня Зиновчук <zinovchukdfuz2@gmail.com>
2024-06-25 18:38:45 +0300

    Add project files.

---
+++ PostalCode/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace PostalCode
+{
+    public partial class Form1 : Form
+    {
+        private string _streetsPath = @"D:\Study\СетевоеПрогр\PostalCodeClient\PostalCodeServer\bin\Debug\streets.xml";
+        private static UdpClient _client;
+        public Form1()
+        {
+            InitializeComponent();
+            _client = new UdpClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XDocument doc = XDocument.Load(_streetsPath);
+            IEnumerable<string> postalCodes = doc.Descendants("IndexPostOfficeStreet")
+                                                .Select(s => s.Element("IndexPostOffice").Value)
+                                                .Distinct()
+                                                .OrderBy(k => int.Parse(k));
+
+            foreach (var item in postalCodes)
+            {
+                comboBox1.Items.Add(item);
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbl_PostalCode.Text = $"Postal Code: {comboBox1.SelectedItem}";
+            SendRequest(comboBox1.SelectedItem.ToString());
+        }
+        private void SendRequest(string postalCode)
+        {
+            try
+            {
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 51111);
+
+                byte[] requestBytes = Encoding.UTF8.GetBytes(postalCode);
+                _client.Send(requestBytes, requestBytes.Length, serverEndPoint);
+
+                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 52222);
+                byte[] responseBytes = _client.Receive(ref remoteEndPoint);
+                string response = Encoding.UTF8.GetString(responseBytes);
+
+                DisplayStreets(response);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DisplayStreets(string response)
+        {
+            string[] streets = response.Split(',');
+            listBox1.Items.Clear();
+            foreach (var street in streets)
+            {
+                listBox1.Items.Add(street);
+            }
+        }
+    }
+}
---
+++ PostalCodeServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PostalCodeServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient(51111);
+            Console.WriteLine("Server is running...");
+
+            while (true)
+            {
+                IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 52222);
+                byte[] buffer = server.Receive(ref clientEndPoint);
+                string postalCode = Encoding.UTF8.GetString(buffer);
+
+                Console.WriteLine($"Postal Code Receiving: {postalCode}");
+
+                List<string> streets = GetStreetsByPostalCode(postalCode);
+                string response = string.Join(",", streets);
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                server.Send(responseBytes, responseBytes.Length, clientEndPoint);
+            }
+        }
+        static List<string> GetStreetsByPostalCode(string postalCode)
+        {
+            XDocument doc = XDocument.Load("streets.xml");
+            var streets = doc.Descendants("IndexPostOfficeStreet")
+                             .Where(s => s.Element("IndexPostOffice").Value == postalCode)
+                             .Select(s => s.Element("Street").Value)
+                             .ToList();
+            return streets;
+        }
+    }
+}
